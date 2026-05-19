9a0588c55805d76450244cf98d562a822f56a687
Anthony Arana <anthony.arana@tecsup.edu.pe>
2026-05-19 17:24:11 -0500

    feat: add ScreenSeries with list, edit and delete composables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
